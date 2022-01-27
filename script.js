ddc2ff05bafa8a61dbe1643742d1c08a1f880230
Dmitry495 <6206507@mail.ru>
2022-01-27 21:31:04 +0300

    first commit

--- script.js
+++ script.js
@@ -12,8 +12,8 @@ icons.forEach(el => {
 
 const popup = document.querySelector('.popup'),
     popupClose = popup.lastElementChild,
-    popupContent = popup.firstElementChild;
-const body = document.querySelector('body');
+    popupContent = popup.firstElementChild,
+    body = document.querySelector('body');
 
 const closePopUp = function (e) {
     e.target.parentElement.style.display = 'none';
@@ -24,10 +24,8 @@ const closePopUp = function (e) {
 //закрывать при нажатие мышки вне поля
 body.addEventListener('click', (e) => {
     e.target.firstElementChild.style.display = 'none';
-          body.style.background = 'none';
+    body.style.background = 'none';
 });
-
-
 
 //закрытие при нажатие кнопки esc
 document.addEventListener('keydown', (e) => {
@@ -47,9 +45,3 @@ const showPopup = function (text) {
 }
 
 popupClose.addEventListener('click', closePopUp);
-
-//#hw1 link(git)
-// 1. Закрывать при нажатие на поле вне картинки 
-// 2. Предусмотреть возможность закрытие по кнопке нажатия на кнопки: esc, alt+f4(preventDefaul) 
-//3.сделать интерфейс в виде lightbox = все элементы за popup становятся затемнеными (кроме одной картинки, которую показывает)
-//4. Запретить промотку если открыта модельное окно
